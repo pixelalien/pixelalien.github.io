53b8e7798db62f40d0de73c2bf7292658b34a82f
Lane Waterman <lane.waterman@emsquared-inc.com>
2023-03-21 15:54:16 -0400

    trial and error

--- js/calculon.js
+++ js/calculon.js
@@ -2,10 +2,10 @@ let valueList = [];
 const buttonWrapper = document.querySelector(".calc-buttons");
 
 function displayClick() {
-	buttonWrapper.addEventListener("click", function () {
+	buttonWrapper.addEventListener("click", function(e) {
 		// if (event.currentTarget )
-		console.log(event.currentTarget)
-	});
+		console.log(e.currentTarget)
+	}, false);
 }
 
 function updateResult(newValue) {
