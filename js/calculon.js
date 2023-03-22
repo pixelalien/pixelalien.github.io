fddc7986149631d725152828b85c2d4cb1a96815
Lane Waterman <lane.waterman@emsquared-inc.com>
2023-03-22 12:33:13 -0400

    number entry

--- js/calculon.js
+++ js/calculon.js
@@ -1,11 +1,31 @@
 let valueList = [];
-const buttonWrapper = document.querySelector(".calc-buttons");
 
-function displayClick() {
-	buttonWrapper.addEventListener("click", function(e) {
-		// if (event.currentTarget )
-		console.log(e.currentTarget)
-	}, false);
+const buttonWrapper = document.querySelector(".calc-buttons");
+let memory = null;
+function operator( memory ) {};
+
+buttonWrapper.addEventListener("click", function(e) {
+	let bVal = parseInt(e.target.innerText);
+	if(bVal === NaN) {
+		let pressed = e.target.innerText;
+		operator(memory, pressed);
+		console.log("pressed: ", pressed);
+	}
+	else {
+		console.log("memory start: ",memory);
+		memory = memory + bVal;
+		console.log("bVal: ",bVal," memory: ",memory);
+	}
+	// if (event.currentTarget )
+	// console.log(e.target.innerText)
+}, false);
+// function displayClick() {
+// }
+const bw = document.querySelector(".calc-buttons")
+function fts() {
+	bw.addEventListener("click", function(){
+		console.log("clickety click")
+	}, false)
 }
 
 function updateResult(newValue) {
