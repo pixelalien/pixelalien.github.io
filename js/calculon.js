0043afd128caadc04f7cb240d7cf2af01ef163c3
Lane Waterman <lane.waterman@emsquared-inc.com>
2023-03-23 14:45:11 -0400

    trying array

--- js/calculon.js
+++ js/calculon.js
@@ -1,20 +1,90 @@
 let valueList = [];
 
 const buttonWrapper = document.querySelector(".calc-buttons");
-let memory = null;
-function operator( memory ) {};
+const lcd = document.querySelector(".result");
+let memory = [];
+let temp1 = null;
+let temp2 = null;
+let lastOp = null;
+
+function displayClick(temp1) {
+	// console.log("display start mem: ", temp1);
+	lcd.innerText = temp1;
+};
+
+function operator( currentNum, memory, pressed ) {
+	if(pressed == "←") {
+		if(currentNum) {
+			console.log("start currentNum: ", currentNum);
+			currentNum = currentNum.substring(0, currentNum.length - 1);
+			console.log('backspace currentNum: ',currentNum);
+			lastOp = "←";
+			return currentNum;
+		}
+		else {
+			console.log("currentNum not set");
+			if(lastOp) {
+				let lastOp = null;
+			}
+		}
+	}
+	else if (pressed === "C") {
+		memory = null;
+		temp1 = null;
+		temp2 = null;
+		lastOp = null;
+		return 0;
+	}
+	else if(pressed == "+") {
+		lastOp = pressed;
+		if(currentNum && memory) {
+			// add the numbers
+			n1 = parseInt(memory);
+			n2 = parseInt(currentNum);
+			nSum = n1 + n2;
+			return nSum;
+		}
+		else if (currentNum) {
+			// set temp value for next number
+			return currentNum;
+		}
+		else {
+			console.log("currentNum not set");
+		}
+	}
+};
 
 buttonWrapper.addEventListener("click", function(e) {
 	let bVal = parseInt(e.target.innerText);
-	if(bVal === NaN) {
+	if(Number.isNaN(bVal)) {
+		// operator button
+		console.log("temp1 start: ",temp1);
 		let pressed = e.target.innerText;
-		operator(memory, pressed);
-		console.log("pressed: ", pressed);
+
+		memory = operator(temp1, memory, pressed);
+		
+		temp1 = null;
+		displayClick(memory);
+		console.log("pressed: ", pressed, "end mem: ", memory);
+		if (lastOp === "←") {
+			
+		}
 	}
 	else {
-		console.log("memory start: ",memory);
-		memory = memory + e.target.innerText;
-		console.log("tVal: ",e.target.innerText," memory: ",memory);
+		// number button
+		console.log("temp1 start: ",temp1, "bVal: ",bVal, "bVal Type: ",typeof(bVal), "input type: ",typeof(e.target.innerText));
+		if(temp1 === null) {
+			temp1 = e.target.innerText;
+			console.log('null click temp1: ',temp1);
+			displayClick(temp1)
+		}
+		else {
+			temp1 = temp1 + e.target.innerText;
+			console.log('click temp1: ',temp1);
+			displayClick(temp1)
+		}
+		
+		console.log("tVal: ",e.target.innerText," temp1: ",temp1);
 	}
 	// if (event.currentTarget )
 	// console.log(e.target.innerText)
